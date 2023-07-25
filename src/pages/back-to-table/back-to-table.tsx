52de3cadf82ace05c08b3d20a19f9e98db25e7ee
HoBager <115457052+HoBager@users.noreply.github.com>
2023-07-25 19:19:40 +0500

    Update back-to-table.tsx

--- src/pages/back-to-table/back-to-table.tsx
+++ src/pages/back-to-table/back-to-table.tsx
@@ -3,7 +3,7 @@ import styles from "./back-to-table.module.css";
 const BackToTable = () => {
   return (
     <div className={styles.body}>
-      <Link to={"/ReactPosts/1"}>
+      <Link to={"/1"}>
         <button
           className={styles.back}
         >{`Вернутся на страницу с таблицей`}</button>
